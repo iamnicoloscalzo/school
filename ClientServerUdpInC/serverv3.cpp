e620c4e66a518e0427be957c02baeb7814e78be2
Singh <38940603+siinghd@users.noreply.github.com>
2018-10-25 17:18:44 +0200

    Update serverv3.cpp

--- ClientServerUdpInC/serverv3.cpp
+++ ClientServerUdpInC/serverv3.cpp
@@ -3,42 +3,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "SocketUDP.hpp"
-#include "Address.hpp"
-#include "mylib.h"
+#include "../classi/SocketUDP.hpp"
+#include "../classi/Address.hpp"
+#include "../lib/mylib.h"
 
-#define IP_LOOP "127.0.0.1" 
+#define IP_LOOP "127.0.0.1"
 #define PORT 8001
 #define BUFSIZE 4096
 #define IP_DHCP "0.0.0.0"
 
-
-
-int main(int argc, char **argv) {
-    if(argc != 3){
-      printf("Usage: %s \nServer IP ,Server PORT\n",argv[0]);
-      exit(1);
+int main(int argc, char** argv)
+{
+    if (argc != 3) {
+        printf("Usage: %s \nServer IP ,Server PORT\n", argv[0]);
+        exit(1);
     }
     int portosend;
-    portosend=atoi(argv[2]);
-    char *ip;
+    portosend = atoi(argv[2]);
+    char* ip;
     int serverport;
-    ip=argv[1];
-    Address mySelf(IP_DHCP,PORT);
+    ip = argv[1];
+    Address mySelf(IP_DHCP, PORT);
     Address addr;
-    Address sendTo_(ip,portosend);
+    Address sendTo_(ip, portosend);
 
+    SocketUDP socket(mySelf);
+    char* buff = socket.ricevi(addr);
+    bool ret = socket.invia(sendTo_, buff);
+    free(buff);
 
-
-
-     SocketUDP socket(mySelf);
-     char* buff= socket.ricevi(addr);
-     bool ret= socket.invia(sendTo_,buff);
-     
-  
     return 0;
 }
