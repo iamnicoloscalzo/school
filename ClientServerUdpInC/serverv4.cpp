fa0abe25e0eb22745ce638f48eca11c0dfce7980
Singh <38940603+siinghd@users.noreply.github.com>
2018-10-25 17:05:52 +0200

    Update serverv4.cpp

--- ClientServerUdpInC/serverv4.cpp
+++ ClientServerUdpInC/serverv4.cpp
@@ -7,9 +7,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include "SocketUDP.hpp"
-#include "Address.hpp"
-#include "mylib.h"
+#include "../classi/SocketUDP.hpp"
+#include "../classi/Address.hpp"
+#include "../lib/mylib.h"
 
 #define IP_LOOP "127.0.0.1" 
 #define PORT 9999
@@ -22,15 +22,20 @@ int main(int argc, char **argv) {
 
     Address mySelf(IP_DHCP,PORT);
     Address addr;
-
+	int ret;
 
 
 
      SocketUDP socket(mySelf);
       
-     char* buff= socket.ricevi(addr);
-	 printf("im here : %s\n",buff);
-     bool ret= socket.invia(addr,buff);
+     char* buff= socket.riceviRaw(addr,&ret);
+	 char* clientToStr=addr.toString();
+   
+     printf("From: %s\n", clientToStr);
+     printf("server ha ricevuto [%d] bytes: %s\n", ret, buff);
+     free(clientToStr);
+	 bool retur= socket.invia(addr,buff);
+	 free(buff);
      
   
     return 0;
