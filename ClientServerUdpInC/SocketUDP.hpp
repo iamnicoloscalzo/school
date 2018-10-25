a9ea703604f3e2f8771d75c2a0c9ab64d0d32e9d
Singh <38940603+siinghd@users.noreply.github.com>
2018-10-25 15:52:53 +0200

    Add files via upload

---
+++ ClientServerUdpInC/SocketUDP.hpp
@@ -0,0 +1,103 @@
+#ifndef __SOCKETUDP_HPP
+#define __SOCKETUDP_HPP
+
+#define BUFSIZE 4096
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "Address.hpp"
+#include "../lib/mylib.h"
+
+class SocketUDP {
+private:       int sockid;
+	
+public:          
+		 SocketUDP();
+                 SocketUDP(Address mySelf);
+		 int sockkid();
+		 bool invia(Address dest ,const char* msg);
+         bool inviaRaw(Address dest ,const char* msg,int numByte);
+		 char* riceviRaw(Address&);			
+	     char* ricevi(Address&);
+		 void nSocketUDP();	
+		~SocketUDP();
+		 
+};
+/******************************/
+int SocketUDP::sockkid(){
+   sockid = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockid < 0) 
+    error("ERRORE non sono riuscito ad aprire il socket");
+    return  sockid;
+}
+
+/******************************/
+
+SocketUDP::SocketUDP(){
+  /*sockid = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockid < 0) 
+    error("ERRORE non sono riuscito ad aprire il socket");*/
+}
+/******************************/
+SocketUDP::SocketUDP(Address mySelf){
+  int socketid = sockkid();
+  printf("%d",socketid);
+  struct sockaddr_in myself_addr = mySelf.getSockaddr_in();
+  if (bind(socketid, (struct sockaddr *) &myself_addr, 
+	   sizeof(struct sockaddr_in)) < 0) {
+    error("ERRORE non sono riuscito in binding");}
+}
+/******************************/
+bool SocketUDP::invia(Address dest ,const char* msg){
+	struct sockaddr_in serveraddr = dest.getSockaddr_in();
+        int ret = sendto(sockid, msg, strlen(msg), 0, 
+	       (struct sockaddr *) &serveraddr, (socklen_t)sizeof(struct sockaddr_in));
+        if (ret < 0) {
+      		return false;
+        }else { 
+                return true;
+	};
+}
+/******************************/
+char* SocketUDP::ricevi(Address& mitt){
+    char buff[BUFSIZE+1];
+    int clientlen = (socklen_t)sizeof(struct sockaddr_in);
+    bzero(buff, BUFSIZE+1);
+    struct sockaddr_in mittente;
+    int ret = recvfrom(sockid, buff, BUFSIZE, 0,
+		 (struct sockaddr *) &mittente, (socklen_t *) &clientlen);
+    if (ret < 0)
+      error("ERRORE in recvfrom");
+    buff[ret]='\0';
+    mitt.setSockaddr_in(mittente);
+    return strdup(buff);
+}
+/******************************/
+bool SocketUDP::inviaRaw(Address dest ,const char* msg, int numByte){
+	struct sockaddr_in serveraddr = dest.getSockaddr_in();
+        int ret = sendto(sockid, msg, numByte, 0, 
+	       (struct sockaddr *) &serveraddr, (socklen_t)sizeof(struct sockaddr_in));
+        if (ret < 0) {
+      		return false;
+        }else { 
+                return true;
+	};
+}
+/******************************/
+/******************************
+void SocketUDP::nSocketUDP(){
+   int ret = close(sockid);
+   if(ret){ error("Errore In chiusura");}else{printf("Socket Chiuso\n");}
+}
+*/
+SocketUDP::~SocketUDP(){
+	close(sockid);
+}
+#endif //__SOCKETUDP_HPP
